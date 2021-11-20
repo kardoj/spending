78ea6973a0b78938e56be6961bf1573458097e31
kardoj <kardoj@gmail.com>
2021-11-20 21:32:01 +0200

    Add new, less intensive, color scheme

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -3,6 +3,23 @@ import 'package:flutter/services.dart';
 
 import 'app/pages/expenses/create_expense_page.dart';
 
+const _colorSchemePrimaryValue = 0xFF8F1E20;
+const MaterialColor _colorSchemePrimarySwatch = MaterialColor(
+  _colorSchemePrimaryValue,
+  <int, Color>{
+    50: Color(0xFFF4E9E9),
+    100: Color(0xFFE9D2D2),
+    200: Color(0xFFDDBCBC),
+    300: Color(0xFFD2A5A6),
+    400: Color(0xFFC78F90),
+    500: Color(0xFFBC7879),
+    600: Color(0xFFB16263),
+    700: Color(0xFFA54B4D),
+    800: Color(_colorSchemePrimaryValue),
+    900: Color(0xFF72181A),
+  }
+);
+
 void main() async {
   runApp(const App());
 }
@@ -18,7 +35,7 @@ class App extends StatelessWidget {
 
     return MaterialApp(
       theme: ThemeData.from(
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: _colorSchemePrimarySwatch)),
       home: const CreateExpensePage()
     );
   }
