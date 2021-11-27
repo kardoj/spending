918e41a1f463b4f7f3a86f8e8e76c899473c70fc
kardoj <kardoj@gmail.com>
2021-11-27 15:41:36 +0200

    Replace input comma with a dot instead of ignoring it

--- lib/src/app/components/forms/money_input_formatter.dart
+++ lib/src/app/components/forms/money_input_formatter.dart
@@ -7,6 +7,17 @@ class MoneyInputFormatter implements TextInputFormatter {
       return newValue;
     }
 
-    return RegExp(r'^(\d+)(\.?)(\d{1,2})?$').hasMatch(newValue.text) ? newValue : oldValue;
+    // We accept only the dot, but let's convert comma to a dot for convenience.
+    final newValueWithCommaReplaced = _replaceCommaWithDot(newValue);
+
+    return RegExp(r'^(\d+)(\.?)(\d{1,2})?$').hasMatch(newValueWithCommaReplaced.text)
+      ? newValueWithCommaReplaced
+      : oldValue;
   }
+
+  TextEditingValue _replaceCommaWithDot(TextEditingValue value)
+    => TextEditingValue(
+      text: value.text.replaceAll(',', '.'),
+      selection: value.selection,
+      composing: value.composing);
 }
